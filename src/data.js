7deca1627c8d9fcbf1ffeb714455306a66bc4864
Italo Simões <2d.italo@gmail.com>
2022-03-09 17:26:51 -0300

    Popular products!

--- src/data.js
+++ src/data.js
@@ -1,6 +1,11 @@
 import banner01 from "./components/img/banner01.png";
 import banner02 from "./components/img/banner02.png";
 import banner03 from "./components/img/banner03.png";
+import banner04 from "./components/img/banner04.png";
+import banner05 from "./components/img/banner05.png";
+import banner06 from "./components/img/banner06.png";
+import banner07 from "./components/img/banner07.png";
+import banner08 from "./components/img/banner08.png";
 
 export const sliderItems = [    
 
@@ -47,5 +52,41 @@ export const categories = [
         img: banner03,
         title: "RETRO GAMING", 
         bg: "fcf13d",       
+    },
+]
+
+export const popularProducts = [
+    
+    {
+        id: 1,
+        img: banner01,                  
+    },
+    {
+        id: 2,
+        img: banner02,            
+    },
+    {
+        id: 3,
+        img: banner03,               
+    },
+    {
+        id: 4,
+        img: banner04,                  
+    },
+    {
+        id: 5,
+        img: banner05,    
+    },
+    {
+        id:6,
+        img: banner06,    
+    },
+    {
+        id: 5,
+        img: banner07,    
+    },
+    {
+        id: 5,
+        img: banner08,    
     },
 ]
