313885a9b6b0083f299ba14b8017f5fa63b52c5f
Italo Simões <2d.italo@gmail.com>
2022-03-09 14:55:28 -0300

    categories completed!

--- src/data.js
+++ src/data.js
@@ -2,13 +2,12 @@ import banner01 from "./components/img/banner01.png";
 import banner02 from "./components/img/banner02.png";
 import banner03 from "./components/img/banner03.png";
 
-export const sliderItems = [
-    
+export const sliderItems = [    
 
     {
         id: 1,
         img: banner01,
-        title: "SUMMER SALE",
+        title: "SUMMER SALE!",
         desc: "PLAY WITH FRIENDS! GET 20% OFF FOR NEW CONSOLES.",
         bg: "f5fafd",
     },
@@ -17,15 +16,36 @@ export const sliderItems = [
         img: banner02,
         title: "POPULAR",
         desc: "PLAY POPULAR GAMES! GET 15% OFF FOR POPULAR CONSOLES.",
-        bg: "fcf13d",
+        bg: "6EFDFD",
     },
     {
         id: 3,
         img: banner03,
         title: "RETRO",
-        desc: "REMEMBER THE CLASSICS! GET OFF FOR RETRO CONSOLES.",
+        desc: "REMEMBER THE CLASSICS! GET 30% OFF FOR RETRO CONSOLES.",
         // bg: "fbf0f4",
-        bg: "6EFDFD",
+        bg: "fcf13d",
     },
 ]
 
+export const categories = [
+    
+    {
+        id: 1,
+        img: banner01,
+        title: "NEW ARRIVALS!",  
+        bg: "f5fafd",      
+    },
+    {
+        id: 2,
+        img: banner02,
+        title: "POPULAR CONSOLES",
+        bg: "6EFDFD",
+    },
+    {
+        id: 3,
+        img: banner03,
+        title: "RETRO GAMING", 
+        bg: "fcf13d",       
+    },
+]
